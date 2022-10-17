2e6920beb62c0136a79f471432352ffd225209a9
tenpei999 <tenpei999@gmail.com>
2022-10-17 19:23:53 +0900

    header html

--- js/main.js
+++
@@ -1,6 +0,0 @@
-window.onload = function(){
-  // ページ読み込み時に実行したい処理
-  // classで取得する場合（最初に現れる要素に追加
-  let elements = document.getElementsByClassName("p-main-visual");
-  elements[0].classList.add("is__road");
-  }
