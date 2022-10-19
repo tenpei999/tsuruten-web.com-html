ebf6ccabea417349ca91fbbe827143f51a7fe80c
tenpei999 <tenpei999@gmail.com>
2022-10-19 18:25:41 +0900

    variable 初期値

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ window.addEventListener('DOMContentLoaded', ()=>{
     () => {
       document.querySelector( 'body' ).classList.toggle( 'is-open' );
       document.querySelector( '.js-hamburger' ).classList.toggle( 'is-open' );
-      document.querySelector( '.p-gmenu__list' ).classList.toggle( 'is-open' );
+      document.querySelector( '.p-gmenu--inner' ).classList.toggle( 'is-open' );
       document.querySelector( '.p-header__background' ).classList.toggle( 'is-open' );
       document.querySelector( '.c-hamburger' ).classList.toggle( 'is-open' );
     }
