25dd900ee3141d5fbcd3877b7a5d6bb5367360d0
tenpei999 <tenpei999@gmail.com>
2022-10-19 11:00:28 +0900

    header リファクタリング前

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ window.addEventListener('DOMContentLoaded', ()=>{
   document.querySelector( '.js-hamburger' ).addEventListener(
     'click',
     () => {
-      document.querySelector( '.js-hamburger' ).classList.toggle( 'is-open' );
-      document.querySelector( '.p-hamburger__list' ).classList.toggle( 'is-open' );
       document.querySelector( 'body' ).classList.toggle( 'is-open' );
+      document.querySelector( '.js-hamburger' ).classList.toggle( 'is-open' );
+      document.querySelector( '.p-gmenu__list' ).classList.toggle( 'is-open' );
+      document.querySelector( '.p-header__background' ).classList.toggle( 'is-open' );
+      document.querySelector( '.c-hamburger' ).classList.toggle( 'is-open' );
     }
   );
 });
