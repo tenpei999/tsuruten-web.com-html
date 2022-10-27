2d7440647be602242e3eba1ce72b12bf06106bc7
tenpei999 <tenpei999@gmail.com>
2022-10-27 21:06:31 +0900

    slidetoggle付加

--- js/main.js
+++ js/main.js
@@ -1,27 +1,7 @@
-window.addEventListener('DOMContentLoaded', ()=>{
-  document.querySelector( '.js-hamburger' ).addEventListener(
-    'click',
-    () => {
-      document.querySelector( 'body' ).classList.toggle( 'is-open' );
-      document.querySelector( '.js-hamburger' ).classList.toggle( 'is-open' );
-      document.querySelector( '.p-gmenu--inner' ).classList.toggle( 'is-open' );
-      document.querySelector( '.p-header__background' ).classList.toggle( 'is-open' );
-      document.querySelector( '.c-hamburger' ).classList.toggle( 'is-open' );
-    }
-  );
-});
-
-//jQuery
-//アコーディオンをクリックした時の動作
-$('.title').on('click', function() {//タイトル要素をクリックしたら
-  var findElm = $(this).next(".box");//直後のアコーディオンを行うエリアを取得し
-  $(findElm).slideToggle();//アコーディオンの上下動作
-    
-  if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
-    $(this).removeClass('close');//クラス名を除去し
-  }else{//それ以外は
-    $(this).addClass('close');//クラス名closeを付与
-  }
+$(function(){
+  $(".p-gmenu__list .item").hover(function(){
+    $(this).children(".middle").stop().slideToggle();
+  });
 });
 
 
