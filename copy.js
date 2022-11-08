fd00b1dea22ef9c10b5aa4d976aae6eb864ef9ad
tenpei999 <tenpei999@gmail.com>
2022-11-09 04:33:00 +0900

    watch,reload成功

--- copy.js
+++ copy.js
@@ -1,34 +1,58 @@
-const gulp = require('gulp');                                    //gulpパッケージを変数に格納
-const watch = require('gulp-watch');
-const plumber = require("gulp-plumber");                         // エラーが発生しても強制終了させない
-const notify = require("gulp-notify");                           // エラー発生時のアラート出力
-const browserSync = require("browser-sync");                     //ブラウザリロード
-const sassGlob = require("gulp-sass-glob-use-forward");
-const sass = require('gulp-sass')(require('sass'));              //gulp-sassパッケージを変数に格納
+const gulp = require('gulp');//gulp本体
 
-//タスクの記述
-gulp.task('sass', function(done) {
-	return gulp.src('./src/scss/style.scss')
-  .pipe(
-    //エラーが出ても処理を止めない
-    plumber({
-      errorHandler: notify.onError('Error:<%= error.message %>')
+//scss
+const sass = require('gulp-dart-sass');//Dart Sass はSass公式が推奨 @use構文などが使える
+const plumber = require("gulp-plumber"); // エラーが発生しても強制終了させない
+const notify = require("gulp-notify"); // エラー発生時のアラート出力
+const browserSync = require("browser-sync"); //ブラウザリロード
+
+
+// 入出力するフォルダを指定
+const srcBase = '../_static/src';
+const assetsBase = '../_assets';
+const distBase = '../_static/dist';
+
+
+const srcPath = {
+  'scss': assetsBase + '/scss/**/*.scss',
+  'html': srcBase + '/**/*.html'
+};
+
+const distPath = {
+  'css': distBase + '/css/',
+  'html': distBase + '/'
+};
+
+/**
+ * sass
+ *
+ */
+const cssSass = () => {
+  return gulp.src(srcPath.scss, {
+    sourcemaps: true
+  })
+    .pipe(
+      //エラーが出ても処理を止めない
+      plumber({
+        errorHandler: notify.onError('Error:<%= error.message %>')
+      }))
+    .pipe(sass({ outputStyle: 'expanded' })) //指定できるキー expanded compressed
+    .pipe(gulp.dest(distPath.css, { sourcemaps: './' })) //コンパイル先
+    .pipe(browserSync.stream())
+    .pipe(notify({
+      message: 'Sassをコンパイルしました！',
+      onLast: true
     }))
-  .pipe( sassGlob() )
-  .pipe(sass({outputStyle: 'expanded'}))
-  .pipe(gulp.dest('./css'))
-  .pipe(browserSync.stream()),
-  done();
-});
+}
+
 
 /**
  * html
  */
 const html = () => {
-  return gulp.src('./**.html')
-    .pipe(gulp.dest('./**.html'))
-  }
-
+  return gulp.src(srcPath.html)
+    .pipe(gulp.dest(distPath.html))
+}
 
 /**
  * ローカルサーバー立ち上げ
@@ -38,7 +62,7 @@ const browserSyncFunc = () => {
 }
 
 const browserSyncOption = {
-  server: "./"
+  server: distBase
 }
 
 /**
@@ -49,35 +73,22 @@ const browserSyncReload = (done) => {
   done();
 }
 
-gulp.task('watch', function(done) {
-  return watch('./src/scss/style.scss', function() {
-    gulp.series(sass)
-    done();
-  });
-});
-
-gulp.task('watch', function(done) {
-  return watch('*/**/', function() {
-      gulp.series(html, browserSyncReload)
-      done();
-  });
-});
+/**
+ *
+ * ファイル監視 ファイルの変更を検知したら、browserSyncReloadでreloadメソッドを呼び出す
+ * series 順番に実行
+ * watch('監視するファイル',処理)
+ */
+const watchFiles = () => {
+  gulp.watch(srcPath.scss, gulp.series(cssSass))
+  gulp.watch(srcPath.html, gulp.series(html, browserSyncReload))
+}
 
 /**
  * seriesは「順番」に実行
  * parallelは並列で実行
  */
 exports.default = gulp.series(
-  gulp.parallel('sass'),
-  gulp.parallel(browserSyncFunc),
-  gulp.parallel('watch'),
+  gulp.parallel(html, cssSass),
+  gulp.parallel(watchFiles, browserSyncFunc)
 );
-
-const browsers = [
-  'last 2 versions',
-  '> 5%',
-  'ie = 11',
-  'ios >= 8',
-  'and_chr >= 5',
-  'Android >= 5',
-]
--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,61 @@
 const gulp = require('gulp')                                    //gulpパッケージを変数に格納
+//scss
+const sass = require('gulp-dart-sass');//Dart Sass はSass公式が推奨 @use構文などが使える
 const sassGlob = require('gulp-sass-glob-use-forward')
-const postcss = require('postcss-scss')
-const sass = require('gulp-dart-sass')       //gulp-sassパッケージを変数に格納
-const autoprefixer = require('autoprefixer')
+const plumber = require("gulp-plumber"); // エラーが発生しても強制終了させない
+const notify = require("gulp-notify"); // エラー発生時のアラート出力
+const browserSync = require("browser-sync"); //ブラウザリロード
 
-const autoprefixerOption = {
-  grid: true
-}
-console.log(postcss)
-
-const postcssOption = [autoprefixer(autoprefixerOption)]
 /**
  * sass
  *
  */
 const cssSass = () => {
   return gulp.src('./src/scss/**/*.scss')
-  .pipe(postcss(postcssOption))
+  .pipe(
+    //エラーが出ても処理を止めない
+    plumber({
+      errorHandler: notify.onError('Error:<%= error.message %>')
+    }))
   .pipe( sassGlob() )
   .pipe(sass({ outputStyle: 'expanded' })) //指定できるキー expanded compressed
   .pipe(gulp.dest('./css'))
+  .pipe(browserSync.stream())
+  .pipe(notify({
+    message: 'Sassをコンパイルしました！',
+    onLast: true
+  }))
+}
+
+/**
+ * html
+ */
+const html = () => {
+  return gulp.src('./index.html')
+  }
+
+/**
+ * ローカルサーバー立ち上げ
+ */
+const browserSyncFunc = () => {
+  browserSync.init(browserSyncOption);
+}
+
+const browserSyncOption = {
+  server: './'
+}
+
+/**
+ * リロード
+ */
+const browserSyncReload = (done) => {
+  browserSync.reload();
+  done();
+}
+
+const watchFiles = () => {
+  gulp.watch('./src/scss/**/*.scss', gulp.series(cssSass))
+  gulp.watch('./index.html', gulp.series(html, browserSyncReload))
 }
 
 /**
@@ -27,5 +63,6 @@ const cssSass = () => {
  * parallelは並列で実行
  */
 exports.default = gulp.series(
-  gulp.parallel(cssSass),
-)
+  gulp.parallel(html, cssSass),
+  gulp.parallel(watchFiles, browserSyncFunc)
+);
