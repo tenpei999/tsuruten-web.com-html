b6e7de7c14a560896ed857534bb2d50b3e6d4faf
tenpei999 <tenpei999@gmail.com>
2022-11-09 16:07:18 +0900

    imagemin css

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,14 @@ const sassGlob = require('gulp-sass-glob-use-forward')
 const plumber = require("gulp-plumber"); // エラーが発生しても強制終了させない
 const notify = require("gulp-notify"); // エラー発生時のアラート出力
 const browserSync = require("browser-sync"); //ブラウザリロード
+const autoprefixer = require('gulp-autoprefixer'); // ベンダープレフィックス付与
+const sourcemaps = require('gulp-sourcemaps'); // ソースマップ出力
+const changed = require('gulp-changed')
+const cached  = require('gulp-cached'); // ファイルキャッシュ
+const imageMin = require('gulp-imagemin')
+const purgecss  = require ( 'gulp-purgecss' )
+const cleanCss  = require ( 'gulp-clean-css' )
+
 
 /**
  * sass
@@ -17,8 +25,14 @@ const cssSass = () => {
     plumber({
       errorHandler: notify.onError('Error:<%= error.message %>')
     }))
+  .pipe(cached('scss')) // ファイルをキャッシュ
+  .pipe(sourcemaps.init())
   .pipe( sassGlob() )
   .pipe(sass({ outputStyle: 'expanded' })) //指定できるキー expanded compressed
+  .pipe(autoprefixer({
+    cascade: true
+  }))
+  .pipe(sourcemaps.write('./'))
   .pipe(gulp.dest('./css'))
   .pipe(browserSync.stream())
   .pipe(notify({
@@ -64,5 +78,31 @@ const watchFiles = () => {
  */
 exports.default = gulp.series(
   gulp.parallel(html, cssSass),
-  gulp.parallel(watchFiles, browserSyncFunc)
+  gulp.parallel(watchFiles, browserSyncFunc),
 );
+
+function imagemin(done) {
+  gulp.src("./src/img/min/*")
+    .pipe(
+      changed("./src/img/**"),
+      imageMin([
+        imageMin.svgo(),
+        imageMin.optipng(),
+        imageMin.gifsicle({ optimizationLevel: 3 }),
+      ])
+    )
+    .pipe(gulp.dest("./dist/img/"));
+  done();
+}
+
+gulp.task('css', () => {
+  return gulp.src('./css/*.css')
+      .pipe(purgecss({
+          content: ['./*.html']
+      }))
+      .pipe(cleanCss())
+      .pipe(gulp.dest('./css/build'))
+})
+
+exports.imagemin = imagemin
+exports.css = gulp.css
